ef2d601da217e5e7e57c37a9fb9b821a28d6e13f
ALKA20MALIK <malikalka2090.com>
2019-09-02 17:30:19 +0530

    Content child

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,7 +7,10 @@ import { Component,
   AfterContentChecked,
   OnDestroy,
   AfterViewInit,
-  AfterViewChecked
+  AfterViewChecked,
+  ViewChild,
+  ElementRef,
+  ContentChild
 } from '@angular/core';
 
 @Component({
@@ -26,12 +29,17 @@ AfterViewInit,
 AfterViewChecked {
 
   @Input('srvElement') element:{type:string,name:String,content:string}
+  @ViewChild('heading',{static:true}) heading:ElementRef
+  @ContentChild('paragraph',{static:true}) paragraph:ElementRef
 
   ngAfterContentInit(){
     console.log('AfterContentInit called')
+    console.log(this.paragraph.nativeElement.textContent)
   }
   ngOnInit() {
     console.log('ngOnInit called')
+    console.log(this.paragraph.nativeElement.textContent)
+    
   }
   constructor() {
     console.log('constructor called')
@@ -48,10 +56,12 @@ AfterViewChecked {
 
   ngAfterViewInit(){
     console.log('ngAfterViewInit called')
+    console.log(this.heading.nativeElement.textContent)
   }
 
   ngAfterViewChecked(){
     console.log('ngAfterViewChecked called')
+    console.log(this.heading.nativeElement.textContent)
   }
 
   ngOnChanges(){
